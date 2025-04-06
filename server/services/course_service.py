ec2c1f427a52ae705fd82ae89a9cf003c9ae75fd
chence <chence1990@gmail.com>
2025-04-05 23:06:57 -0400

    update

--- server/services/course_service.py
+++ server/services/course_service.py
@@ -146,6 +146,7 @@ class CourseService:
                 e.student_id,
                 g.id grade_id,
                 g.final_grade,
+                g.final_gpa,
                 g.feedback grade_feedback
             from term_program_courses tpc
             join terms t on tpc.term_id=t.id
